257bd1c189b653cd5d6d5c85bdca389453e1dc53
sjl <913589581@qq.com>
2019-04-29 20:55:10 +0800

    add note

--- main.cpp
+++ main.cpp
@@ -23,87 +23,57 @@
 #include "FATFileSystem.h"
 #include "LittleFileSystem.h"
 
-// Default network interface object. Don't forget to change the WiFi SSID/password in mbed_app.json if you're using WiFi.
+//默认网络接口对象实例声明
 NetworkInterface *net = NetworkInterface::get_default_instance();
 
-// Default block device available on the target board
+//扩展存储实例
 BlockDevice *bd = BlockDevice::get_default_instance();
 
 #if COMPONENT_SD || COMPONENT_NUSD
-// Use FATFileSystem for SD card type blockdevices
 FATFileSystem fs("fs");
 #else
-// Use LittleFileSystem for non-SD block devices to enable wear leveling and other functions
 LittleFileSystem fs("fs");
 #endif
 
-// Default User button for GET example
-InterruptIn button(BUTTON1);
-
+//火灾预警系统传感器主要参数
 #define SENSORS_POLL_INTERVAL 3.0
 #define YANWU_GATE 70.0
 #define TEMP_GATE  70.0
 
-//自定义输入引脚
-DS1820      ds1820(D7); 
+//传感器定义
+DS1820   ds1820(D7); 
 AnalogIn adc_yanwu(A1);
 float temp = 0;
 
+//服务器传感器资源
 MbedCloudClientResource *res_yanwu;
 MbedCloudClientResource *res_temp;
 
-// Declaring pointers for access to Pelion Device Management Client resources outside of main()
-MbedCloudClientResource *res_button;
-
-// An event queue is a very useful structure to debounce information between contexts (e.g. ISR and normal threads)
-// This is great because things such as network operations are illegal in ISR, so updating a resource in a button's fall() function is not allowed
 EventQueue eventQueue;
 
-// When the device is registered, this variable will be used to access various useful information, like device ID etc.
+//存储终端设备信息
 static const ConnectorClientEndpointInfo* endpointInfo;
 
-/**
- * Button handler
- * This function will be triggered either by a physical button press or by a ticker every 5 seconds (see below)
- */
-void button_press() {
-    int v = res_button->get_value_int() + 100;
-    res_button->set_value(v);
-    printf("Button clicked %d times\n", v);
-}
-
-/**
- * Notification callback handler
- * @param resource The resource that triggered the callback
- * @param status The delivery status of the notification
- */
-void button_callback(MbedCloudClientResource *resource, const NoticationDeliveryStatus status) {
-    printf("Button notification, status %s (%d)\n", MbedCloudClientResource::delivery_status_to_string(status), status);
-}
-
-/**
- * Registration callback handler
- * @param endpoint Information about the registered endpoint such as the name (so you can find it back in portal)
- * When the device is registered, this variable will be used to access various useful information, like device ID etc.
- */
+//注册到服务器时候的回调函数
 void registered(const ConnectorClientEndpointInfo *endpoint) {
     printf("Registered to Pelion Device Management. Endpoint Name: %s\n", endpoint->internal_endpoint_name.c_str());
     endpointInfo = endpoint;
 }
 
+//传感器数据读取和计算
 void sensors_update() {
     float yanwu = adc_yanwu.read()*100;
     int   result = 0; 
     float temp_first = 0;
     if (ds1820.begin()) {
-        ds1820.startConversion();   // start temperature conversion from analog to digital
-        wait(1.0);                  // let DS1820 complete the temperature conversion
-        result = ds1820.read(temp_first); // read temperature from DS1820 and perform cyclic redundancy check (CRC)
+        ds1820.startConversion();
+        wait(1.0);
+        result = ds1820.read(temp_first);
         if (result==0)
             temp=temp_first;
     }
-    printf("yanwu :  %6.4f ,  temp: %6.4f C\r\n", yanwu, temp);
-    if(yanwu>YANWU_GATE||temp>TEMP_GATE)
+    printf("yanwu :  %6.4f %%,  temp: %6.4f C\r\n", yanwu, temp);
+    if( yanwu>YANWU_GATE || temp>TEMP_GATE )
         printf("Fire!!!Fire!!!Fire!!! \r\n");
     if (endpointInfo) {
         res_yanwu->set_value(yanwu);
@@ -112,13 +82,13 @@ void sensors_update() {
 }
 
 int main(void) {
-    printf("\nStarting Simple Pelion Device Management Client example\n");
+    printf("\nStarting Fire Alarm System\n");
 
+    //挂载SD卡
     int storage_status = fs.mount(bd);
     if (storage_status != 0) {
         printf("Storage mounting failed.\n");
     }
-
     if (storage_status) {
         printf("Formatting the storage...\n");
         int storage_status = StorageHelper::format(&fs, bd);
@@ -127,7 +97,7 @@ int main(void) {
         }
     } 
 
-    // Connect to the Internet (DHCP is expected to be on)
+    //连接网络
     printf("Connecting to the network using the default network interface...\n");
     net = NetworkInterface::get_default_instance();
     nsapi_error_t net_status = NSAPI_ERROR_NO_CONNECTION;
@@ -137,27 +107,18 @@ int main(void) {
             printf("Unable to connect to network (%d). Retrying...\n", net_status);
         }
     }
-
     printf("Connected to the network successfully. IP address: %s\n", net->get_ip_address());
 
+    //客户端对象初始化
     printf("Initializing Pelion Device Management Client...\n");
-
-    // SimpleMbedCloudClient handles registering over LwM2M to Pelion Device Management
     SimpleMbedCloudClient client(net, bd, &fs);
     int client_status = client.init();
     if (client_status != 0) {
         printf("Pelion Client initialization failed (%d)\n", client_status);
         return -1;
     }
-
-    // Creating resources, which can be written or read from the cloud
-    res_button = client.create_resource("3200/0/5501", "Button Count");
-    res_button->set_value(0);
-    res_button->methods(M2MMethod::GET);
-    res_button->observable(true);
-    res_button->attach_notification_callback(button_callback);
     
-    // Sensor resources
+    //传感器资源初始化
     res_yanwu = client.create_resource("3303/0/5700", "Yanwu_ADC");
     res_yanwu->set_value(0);
     res_yanwu->methods(M2MMethod::GET);
@@ -168,23 +129,17 @@ int main(void) {
     res_temp->methods(M2MMethod::GET);
     res_temp->observable(true);
 
+    //注册到Mbed Pelion 服务器
     printf("Initialized Pelion Device Management Client. Registering...\n");
-
-    // Callback that fires when registering is complete
     client.on_registered(&registered);
-
-    // Register with Pelion DM
     client.register_and_connect();
 
-    // The button fires on an interrupt context, but debounces it to the eventqueue, so it's safe to do network operations
-    button.fall(eventQueue.event(&button_press));
-    printf("Press the user button to increment the LwM2M resource value...\n");
-
+    //根据传感器设置轮询时间更新数据
     Ticker timer;
     timer.attach(eventQueue.event(&sensors_update), SENSORS_POLL_INTERVAL);
 
-    // You can easily run the eventQueue in a separate thread if required
+    // 队列轮训
     eventQueue.dispatch_forever();
 }
 
-#endif /* MBED_TEST_MODE */
+#endif
